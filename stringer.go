f7da5263092dc14fc7bdcb1320d1ea9dbe816821
Geoff Pitfield <geoff@pitfield.com>
2016-07-13 16:24:22 -0700

    add Stringer interface for Histogram, Bin

---
+++ stringer.go
@@ -0,0 +1,17 @@
+package ghist
+
+import "fmt"
+
+func (h Histogram) String() (str string) {
+	str = fmt.Sprintf("%d bin ghist summarizing %d items\n", len(h.Bins), h.Count)
+	str += fmt.Sprintf("Mean: %2f    Median: %2f\n", h.Mean(), h.Median())
+	str += fmt.Sprintf("Mode: %v\n\nBins:\n", h.Mode().String())
+	for i, bin := range h.Bins {
+		str += fmt.Sprintf("%d: %v\n", i, bin.String())
+	}
+	return
+}
+
+func (b Bin) String() string {
+	return fmt.Sprintf("%d in [%2f:%2f] totaling %2f", b.Count, b.Max, b.Min, b.Sum)
+}
